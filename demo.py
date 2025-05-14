b9fe7c5b4a7c81ecba3e1cc2e8d27ebbdc5bf5d3
souvikpaul1 <souvikpaul425@gmail.com>
2025-05-14 21:31:24 +0530

    added data injestion from MongoDB

--- demo.py
+++ demo.py
@@ -1,11 +1,11 @@
 # below code is to check the logging config
 from src.logger import logging
 
-logging.debug("This is a debug message.")
-logging.info("This is an info message.")
-logging.warning("This is a warning message.")
-logging.error("This is an error message.")
-logging.critical("This is a critical message.")
+# logging.debug("This is a debug message.")
+# logging.info("This is an info message.")
+# logging.warning("This is a warning message.")
+# logging.error("This is an error message.")
+# logging.critical("This is a critical message.")
 
 # --------------------------------------------------------------------------------
 
@@ -22,7 +22,7 @@ logging.critical("This is a critical message.")
 
 # --------------------------------------------------------------------------------
 
-# from src.pipline.training_pipeline import TrainPipeline
+from src.pipline.training_pipeline import TrainPipeline
 
-# pipline = TrainPipeline()
-# pipline.run_pipeline()
+pipline = TrainPipeline()
+pipline.run_pipeline()
